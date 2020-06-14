22c35089e5b13335295eb24dfc5e84af1307835f
Evgeny Busalaev <refresh@johnny.local>
2020-06-14 23:09:54 +0300

    feat: добавить закрытие попапа при клике вне окна

--- script/script.js
+++ script/script.js
@@ -1,38 +1,42 @@
 // Находим элементы в DOM
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
+let popup = document.querySelector('.popup')
+let editButton = document.querySelector('.profile__edit-button')
+let closeButton = document.querySelector('.popup__close-button')
+let nameInput = document.querySelector('.popup__input_name')
+let jobInput = document.querySelector('.popup__input_job')
+let formElement = document.querySelector('.popup__form');
 
 // Выбираем элементы, куда должны быть вставлены значения полей
-let profileHeader = document.querySelector('.profile__header');
-let profileSubheader = document.querySelector('.profile__subheader');
+let profileHeader = document.querySelector('.profile__header')
+let profileSubheader = document.querySelector('.profile__subheader')
 
 // Функция открывает/закрывает popup и вставляет значения в input
 function getPopup() {
 
-    popup.classList.toggle('popup_opened');
+    popup.classList.toggle('popup_opened')
 
     if (popup.classList.contains('popup_opened')) {
-    nameInput.value = profileHeader.textContent;
-    jobInput.value = profileSubheader.textContent;
+    nameInput.value = profileHeader.textContent
+    jobInput.value = profileSubheader.textContent
   }
 }
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
+function closeOverlay() {
+  if (event.target !== event.currentTarget) { return }
+  getPopup()
+}
 
 function formSubmitHandler (evt) {
-     evt.preventDefault();
+     evt.preventDefault()
 
     // Вставьте новые значения с помощью textContent
-    profileHeader.textContent = nameInput.value;
-    profileSubheader.textContent = jobInput.value;
+    profileHeader.textContent = nameInput.value
+    profileSubheader.textContent = jobInput.value
 
     getPopup();
 }
 
-editButton.addEventListener('click', getPopup);
-closeButton.addEventListener('click', getPopup);
-formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', getPopup)
+closeButton.addEventListener('click', getPopup)
+formElement.addEventListener('submit', formSubmitHandler)
+popup.addEventListener('click', closeOverlay)
