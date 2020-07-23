9e74276247882c4ba9512871b75edde955f0c6f9
Evgeny Busalaev <refresh@johnny.local>
2020-07-23 13:30:55 +0300

    feat: 7-я проектная работа до первого ревью

--- script/index.js
+++ script/index.js
@@ -1,5 +1,10 @@
 "use strict"
 
+import { config } from './validationConfig.js'
+import { enableValidation } from './formValidator.js'
+import { initialCards } from './initialCardsData.js'
+import { Card } from './card.js'
+
 // Находим элементы в DOM
 const allPopups = document.querySelectorAll('.popup')
 
@@ -13,10 +18,6 @@ const addPopupTitleInput = addPopup.querySelector('.popup__input_title')
 const addPopupLinkInput = addPopup.querySelector('.popup__input_link')
 const addPopupForm = addPopup.querySelector('.popup__add-form')
 
-const zoomPopup = document.querySelector('.popup_zoom')
-const zoomPopupImage = zoomPopup.querySelector('.popup__image')
-const zoomPopupCaption = zoomPopup.querySelector('.popup__caption')
-
 const profile = document.querySelector('.profile')
 
 const addButton = profile.querySelector('.profile__add-button')
@@ -25,76 +26,27 @@ const profileHeader = profile.querySelector('.profile__header')
 const profileSubheader = profile.querySelector('.profile__subheader')
 
 const photoGrid = document.querySelector('.photo-grid')
-const cardTemplateContent = photoGrid.querySelector('.photo-grid__template').content
 
-const config = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_inactive',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input-error_active'
-}
+const renderElements = () => {
+  initialCards.forEach((item) => {
+    const card = new Card(item)
+    const cardElement = card.generateCard();
+    photoGrid.append(cardElement);
+  });
+};
 
-// Из этого массива создаем галерею
-
-const initialCards = [
-  {name: 'Ольхон', 
-  link: 'images/photo-grid-olkhon.jpg',
-  alt: 'Каменная глыба посреди замерзжего озера'},
-  {name: 'Эльбрус', 
-  link: 'images/photo-grid-elbrus.jpg',
-  alt: 'Вид на гору издали на закате солнца'},
-  {name: 'Домбай',
-  link: 'images/photo-grid-dombai.jpg',
-  alt: 'Горные вершины Домбая'},
-  {name: 'Камчатка',
-  link: 'images/photo-grid-kamchatka.jpg',
-  alt: 'Снежная горная вершина с высоты птичьего полета'},
-  {name: 'Клин',
-  link: 'images/photo-grid-klin.jpg',
-  alt: 'Заброшенная здание церкви в поле'},
-  {name: 'Хибины',
-  link: 'images/photo-grid-khibiny.jpg',
-  alt: 'Заснеженные горные склоны'}
-];
-
-// Создаем карточку и добавляем путь картинки, альт-текст и название карточки.
-
-const createCard = (cardItem) => {
-    const card = cardTemplateContent.cloneNode(true)
-    card.querySelector('.photo-grid__image').src = cardItem.link
-    card.querySelector('.photo-grid__image').alt = cardItem.alt
-    card.querySelector('.photo-grid__caption').textContent = cardItem.name
-    return card
-  }
-
-// Создаем объект и передаем его для создания карточки
-
-const addCard = () => {
-  const cardItem = {
+const addUserCard = () => {
+  const item = {
     name: addPopupTitleInput.value, 
     link: addPopupLinkInput.value,
     value: addPopupTitleInput.value
   }
-  photoGrid.prepend(createCard(cardItem))
+  const card = new Card(item)
+  const cardElement = card.generateCard();
+  photoGrid.prepend(cardElement);
 }
 
-// Удаляет карточку при нажатии на "мусорку"
-
-const removeCard = (event) => {
-  if (event.target.classList.contains('photo-grid__trash-button')) {
-    event.target.closest('.photo-grid__item').remove()
-  }
-}
-
-// Активируем/деактивируем "лайк"
-
-const toggleLike = (event) => {
-  if (event.target.classList.contains('photo-grid__like-button')) {
-    event.target.classList.toggle('liked')
-  }
-}
+renderElements();
 
 // Делаем видимыми/невидимыми попапы добавлением класса
 
@@ -107,8 +59,6 @@ const closePopup = (popupName) => {
   removeEscListener()
   popupName.classList.remove('popup_opened')
 }
-
-// Закрываем попапы при нажатии "Esc" и вызываем функцию удаления слушателя.
 
 const closePopupOnEsc = (event) => {
   if (event.key === "Escape") { 
@@ -125,25 +75,12 @@ if (event.target.classList.contains('popup__close-button') ||
   }
 }))
 
-// Сбрасываем ошибки в случае если клиент закрыл ошибочную форму и открыл опять
-
-const resetInputsErrors = (popupName, config) => {
-  const formElement = popupName.querySelector(config.formSelector)
-  const inputElements = popupName.querySelectorAll(config.inputSelector)
-  const inputsList = Array.from(inputElements)
-  inputElements.forEach(inputElement => {
-      hideInputError(formElement, inputElement, config)
-    })
-  const buttonElement = popupName.querySelector(config.submitButtonSelector)
-  toggleButtonState(inputsList, buttonElement, config)
-}
-
 // При сабмите формы добавления, убираем ее с экрана и вызываем функцию добавления карточки
 
 const addFormSubmitHandler = (event) => {
   event.preventDefault()
-  closePopup(addPopup)
-  addCard()
+    closePopup(addPopup)
+    addUserCard()
 }
 
 // При сабмите формы редактирования профиля убираем ее с экрана и добавляем значения полей в нужные места
@@ -160,57 +97,90 @@ const editFormSubmitHandler = (event) => {
 const addInputValue = (popupName) => {
   editPopupNameInput.value = profileHeader.textContent
   editPopupJobInput.value = profileSubheader.textContent
-  resetInputsErrors(popupName, config)
+  enableValidation(config)
 }
 
 // Очищаем инпуты
 
 const clearInputs = (popupName) => {
   addPopupForm.reset()
-  resetInputsErrors(popupName, config)
+  enableValidation(config)
 }
-
-// Добавляем src и описание картинки
-
-const zoomPopupImages = (event) => {
-  if (event.target.classList.contains('photo-grid__image')) {
-    zoomPopupImage.src = event.target.src
-    zoomPopupCaption.textContent = event.target.closest('.photo-grid__item').innerText
-    openPopup(zoomPopup)
-  }
-}
-
-// Собираем галерею карточек с фотографиями на загрузке страницы из массива
-
-initialCards.forEach((cardItem) => {
-  photoGrid.append(createCard(cardItem))
-})
 
 // Слушатели событий
-
-profileEditButton.addEventListener('click', () => {
-  addInputValue(editPopup)
-  openPopup(editPopup)
-})
 
 addButton.addEventListener('click', () => {
   clearInputs(addPopup)
   openPopup(addPopup)
 })
 
-photoGrid.addEventListener('click', zoomPopupImages)
-photoGrid.addEventListener('click', toggleLike)
-photoGrid.addEventListener('click', removeCard)
+profileEditButton.addEventListener('click', () => {
+  addInputValue(editPopup)
+  openPopup(editPopup)
+})
 
 editPopupForm.addEventListener('submit', editFormSubmitHandler)
 addPopupForm.addEventListener('submit', addFormSubmitHandler)
 
-const addEscListener = () => {
+export const addEscListener = () => {
   document.addEventListener('keydown', closePopupOnEsc)
 }
 
-const removeEscListener = () => {
+export const removeEscListener = () => {
   document.removeEventListener('keydown', closePopupOnEsc)
 }
 
-enableValidation(config);
+// class Popup {
+//   constructor (popupType) {
+//     this._popupType = popupType
+//   }
+  
+//   handleOpenPopup() {
+//     popupElement.querySelector('.popup__image').src = this._link
+//     popupElement.querySelector('.popup__caption').textContent = this._name
+//     popupElement.querySelector('.popup__image').alt = this._alt
+//     popupElement.classList.add('popup_opened');
+//   }
+
+//   _handleClosePopup() {
+//     popupElement.classList.remove('popup_opened');
+//   }
+// }
+
+// popupOpenButtons.forEach(popupOpenButton => { 
+//   popupOpenButton.addEventListener('click', () => {
+//   if (event.target.classList.contains('profile__edit-button')) {
+//     const getPopup = new Popup(editPopup)
+//     // addInputValue(editPopup)
+//     getPopup.handleOpenPopup()
+//   }
+//   // if (event.target.classList.contains('profile__add-button')) {}
+//   //   const getPopup = new Popup(addPopup)
+//   //   clearInputs(addPopup)
+//   //   getPopup._handleOpenPopup(editPopup)
+//   })
+// })
+
+// Записываем в переменную все формы на странице, для каждой формы делаем отмену дефолтного поведения на submit передаем форму в функцию установки обработчиков на инпуты.
+
+// class Popup extends Card {
+//   _setEventListeners() {
+//     this._element.closest('.popup').addEventListener('click', () => {
+//       if (event.target == event.currentTarget)
+//       super._handleClosePopup(event.currentTarget);
+//     });
+//   }
+// }
+
+// Сбрасываем ошибки в случае если клиент закрыл ошибочную форму и открыл опять
+
+// const resetInputsErrors = (popupName, config) => {
+//   const formElement = popupName.querySelector(config.formSelector)
+//   const inputElements = popupName.querySelectorAll(config.inputSelector)
+//   const inputsList = Array.from(inputElements)
+//   inputElements.forEach(inputElement => {
+//       _hideInputError(formElement, inputElement, config)
+//     })
+//   const buttonElement = popupName.querySelector(config.submitButtonSelector)
+//   _toggleButtonState(inputsList, buttonElement, config)
+// }
---
+++ script/initialCardsData.js
@@ -0,0 +1,20 @@
+export const initialCards = [
+    {name: 'Ольхон', 
+    link: 'images/photo-grid-olkhon.jpg',
+    alt: 'Каменная глыба посреди замерзжего озера'},
+    {name: 'Эльбрус', 
+    link: 'images/photo-grid-elbrus.jpg',
+    alt: 'Вид на гору издали на закате солнца'},
+    {name: 'Домбай',
+    link: 'images/photo-grid-dombai.jpg',
+    alt: 'Горные вершины Домбая'},
+    {name: 'Камчатка',
+    link: 'images/photo-grid-kamchatka.jpg',
+    alt: 'Снежная горная вершина с высоты птичьего полета'},
+    {name: 'Клин',
+    link: 'images/photo-grid-klin.jpg',
+    alt: 'Заброшенная здание церкви в поле'},
+    {name: 'Хибины',
+    link: 'images/photo-grid-khibiny.jpg',
+    alt: 'Заснеженные горные склоны'}
+  ];
